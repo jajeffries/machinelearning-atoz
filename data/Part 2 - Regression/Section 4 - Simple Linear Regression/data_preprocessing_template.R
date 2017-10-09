e0fdd8fd1f4611ccd77e60c2adbbef41a55ad5be
James Jeffries <jamesjeffries1@gmail.com>
2017-10-09 19:43:37 +0100

    started part 2

--- data/Part 2 - Regression/Section 4 - Simple Linear Regression/data_preprocessing_template.R
+++ data/Part 2 - Regression/Section 4 - Simple Linear Regression/data_preprocessing_template.R
@@ -1,4 +1,4 @@
-# Data Preprocessing
+# Data Preprocessing Template
 
 # Importing the dataset
 dataset = read.csv('Data.csv')
@@ -7,10 +7,10 @@ dataset = read.csv('Data.csv')
 # install.packages('caTools')
 library(caTools)
 set.seed(123)
-split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
 # Feature Scaling
-# training_set[,2:3] = scale(training_set[,2:3])
-# test_set[,2:3] = scale(test_set[,2:3])
+# training_set = scale(training_set)
+# test_set = scale(test_set)
